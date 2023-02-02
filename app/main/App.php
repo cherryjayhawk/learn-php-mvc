77153d670fc17cc95910d648d86793cafe0b45c3
cherryjayhawk <muhbintangr19@gmail.com>
2023-02-02 10:38:08 +0700

    Routing, membersihkan url, dan membatasi akses user ke folder app

--- app/main/App.php
+++ app/main/App.php
@@ -3,6 +3,22 @@
 class App
 {
     public function __construct(){
-        echo 'BRUH';
+        $bruh = $this->parseUrl();
+        var_dump($bruh);
     }
+
+    public function parseUrl()
+    {
+        if (isset($_GET['bruh'])) {
+            // menghilangkan '/' diakhir url
+            $url = rtrim($_GET['bruh']);
+            // memfilter url dari karakter random
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // memecah url delimiter: '/' dan mengkonversi string2 menjadi elemen array index
+            $url = explode('/', $url);
+
+            return $url;
+        }
+    }
+
 }
